b7521a6d759354a6a3d6396d32e9bb235bf09d1f
Geoff Ryan <geoffrey.s.ryan@gmail.com>
2019-10-02 13:36:28 -0400

    Added multiband light curve batch example

--- examples/plotMultibandLightCurveBatch.py
+++ examples/plotMultibandLightCurveBatch.py
@@ -20,21 +20,25 @@ dL = 1.23e26
 Y = np.array([thV, E0, thC, thW, b, 0, 0, 0, n0, p, epse, epsB, ksiN, dL])
 
 # Time and Frequencies
+Nt = 100
+Nnu = 3
+t = np.empty((Nt, Nnu))
+nu = np.empty((Nt, Nnu))
+
 ta = 1.0e-1 * grb.day2sec
 tb = 1.0e3 * grb.day2sec
-t = np.geomspace(ta, tb, num=100)
-
 nuR = 6.0e9
 nuO = 1.0e14
 nuX = 1.0e18
 
+t[:, :] = np.geomspace(ta, tb, num=100)[:, None]
+nu[:, 0] = nuR
+nu[:, 1] = nuO
+nu[:, 2] = nuX
+
 # Calculate!
-print("Calc Radio")
-FnuR = grb.fluxDensity(t, nuR, jetType, specType, *Y)
-print("Calc Optical")
-FnuO = grb.fluxDensity(t, nuO, jetType, specType, *Y)
-print("Calc X-ray")
-FnuX = grb.fluxDensity(t, nuX, jetType, specType, *Y)
+print("Calculate!")
+Fnu = grb.fluxDensity(t, nu, jetType, specType, *Y)
 
 # Plot!
 print("Plot")
@@ -42,9 +46,9 @@ print("Plot")
 tday = t * grb.sec2day
 
 fig, ax = plt.subplots(1, 1)
-ax.plot(tday, FnuR, ls='-', label=r'$\nu=6$ GHz')
-ax.plot(tday, FnuO, ls='--', label=r'$\nu=10^{14}$ Hz')
-ax.plot(tday, FnuX, ls='-.', label=r'$\nu=10^{18}$ Hz')
+ax.plot(tday[:, 0], Fnu[:, 0], ls='-', label=r'$\nu=6$ GHz')
+ax.plot(tday[:, 1], Fnu[:, 1], ls='--', label=r'$\nu=10^{14}$ Hz')
+ax.plot(tday[:, 2], Fnu[:, 2], ls='-.', label=r'$\nu=10^{18}$ Hz')
 ax.set_xscale('log')
 ax.set_yscale('log')
 ax.set_xlabel(r'$t$ (d)')
@@ -52,6 +56,6 @@ ax.set_ylabel(r'$F_\nu$ (mJy)')
 ax.legend()
 fig.tight_layout()
 
-print("Saving lc_multi.png")
-fig.savefig("lc_multi.png")
+print("Saving lc_multi_batch.png")
+fig.savefig("lc_multi_batch.png")
 plt.close(fig)
