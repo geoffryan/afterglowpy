0a50a440ed062e0d6aff5ad8500657384172540a
Geoff Ryan <geoffrey.s.ryan@gmail.com>
2020-02-26 12:58:40 -0500

    more docs

--- afterglowpy/__init__.py
+++ afterglowpy/__init__.py
@@ -45,12 +45,13 @@ from .version import __version__
 from . import shock
 from . import cocoon
 from . import jet
+from . import flux
 from .flux import fluxDensity, intensity
 from .cocoon import (Hz2eV, Msun, c, cgs2mJy, day2sec, eV2Hz, ee, h, hbar,
                      mJy2cgs, me, mp, parsec, sec2day, sigmaT)
 
 __all__ = ['__version__',
-           'shock', 'cocoon', 'jet', 'fluxDensity', 'intensity',
+           'shock', 'cocoon', 'jet', 'flux', 'fluxDensity', 'intensity',
            'Hz2eV', 'Msun', 'c', 'cgs2mJy', 'day2sec', 'eV2Hz', 'ee', 'h',
            'hbar', 'mJy2cgs',
            'me', 'mp', 'parsec', 'sec2day', 'sigmaT']
