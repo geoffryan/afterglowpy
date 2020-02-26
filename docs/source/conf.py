f4c188466ca389d99cba6945e3021d96f08cf7b3
Geoff Ryan <geoffrey.s.ryan@gmail.com>
2020-02-25 20:18:00 -0500

    again

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@
 
 import os
 import sys
-# sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../..'))
 # from afterglowpy.version import __version__
 
 # -- Project information -----------------------------------------------------
